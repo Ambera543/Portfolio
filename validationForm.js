6c00ea6ca024ba014e1e20d38f3ed83ac342a80d
Ambera543 <g.eismontiene@gmail.com>
2022-02-03 12:03:27 +0200

    buttons

--- validationForm.js
+++ validationForm.js
@@ -1,31 +1,24 @@
+var flag = 0;
+function username() {
+  user = loginForm.username.value;
+  if (user == "") {
+    document.getElementById("error0").innerHTML = "Enter Username";
+    flag = 1;
+  }
+// else {document.getElementById("error0").reset();}
+}
+function password() {
+  pass = loginForm.password.value;
+  if (pass == "") {
+    document.getElementById("error1").innerHTML = "Enter password";
+    flag = 1;
+  }
+}
 
-var flag=0;
-        function username()
-        {
-            user=loginForm.username.value;
-            if(user=="")
-            {
-                document.getElementById("error0").innerHTML="Enter Username";
-                flag=1;
-            }
-        }   
-        function password()
-        {
-            pass=loginForm.password.value;
-            if(pass=="")
-            {
-                document.getElementById("error1").innerHTML="Enter password";   
-                flag=1;
-            }
-        }
-
-        function check(form)
-        {
-            flag=0;
-            username();
-            password();
-            if(flag==1)
-                return false;
-            else
-                return true;
-        }
+function check(form) {
+  flag = 0;
+  username();
+  password();
+  if (flag == 1) return false;
+  else return true;
+}
